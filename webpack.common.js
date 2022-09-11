b144775320f6d9e2e440ed2eac6773909200fb42
Leon Lonsdale <leonlonsdale@icloud.com>
2022-09-11 11:22:25 +0100

    responsive sass, mixins, basic nav custom property skeleton

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.scss',
+      chunkFilename: 'main.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      title: 'Webpack Testing',
+    }),
+  ],
+};
